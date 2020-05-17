6e5969c0f934d7c92a6542f155b288d0e566d55d
Leroy Truong <leroytruong@gmail.com>
2020-05-17 20:15:53 +0200

    feat(nav): implement navigation routing
    * feat(nav): implement a side navigation bar
    
    * feat(nav): implement navigation routing between several components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MatListModule } from '@angular/material/list';
+import { RpiComponent } from './rpi/rpi.component';
+import { TournamentComponent } from './tournament/tournament.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    RpiComponent,
+    TournamentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
