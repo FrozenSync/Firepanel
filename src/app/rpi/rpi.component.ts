10b0f7d1b4c1e658c9539fe0c2894e8dbff8aaff
Leroy Truong <leroytruong@gmail.com>
2020-05-18 10:42:37 +0200

    feat(rpi): implement registration and overview list

--- src/app/rpi/rpi.component.ts
+++ src/app/rpi/rpi.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-rpi',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RpiComponent implements OnInit {
 
-  constructor() { }
+  userId: string;
+
+  id: string;
+  name: string;
+
+  pis: Observable<any[]>;
+
+  constructor(private auth: AngularFireAuth, private firestore: AngularFirestore) {
+    this.auth.user.subscribe(user => {
+      this.userId = user.uid;
+      this.pis = firestore.collection('raspberryPis', ref => ref.where('ownerId', '==', this.userId)).valueChanges();
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    console.log(this.id);
+    const doc = this.firestore.collection('raspberryPis').doc(this.id);
+    doc.update({
+      ownerId: this.userId,
+      name: this.name
+    }).catch(err => console.log(err));
+  }
 }
