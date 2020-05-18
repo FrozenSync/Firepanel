fad70baeedbf85b4eeebdd212a2e03a9529cd55c
leroy <leroytruong@gmail.com>
2020-05-18 12:57:51 +0200

    feat(hosting): enable Firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDz4ySEwQdaXFHN7KnUKmzSvFti1o0qOaE',
+    authDomain: 'firebridge-7ba35.firebaseapp.com',
+    databaseURL: 'https://firebridge-7ba35.firebaseio.com',
+    projectId: 'firebridge-7ba35',
+    storageBucket: 'firebridge-7ba35.appspot.com',
+    messagingSenderId: '494567369272',
+    appId: '1:494567369272:web:cf93d3b00d0f383da354cd'
+  },
+  actionCodeSettings: {
+    url: 'https://firebridge-7ba35.web.app/login',
+    handleCodeInApp: true
+  }
 };
